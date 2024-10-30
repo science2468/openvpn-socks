9e44fad9df51d28246f3467b1ead5cf0e7ae29fb
山茱萸 <102643084+science2468@users.noreply.github.com>
2024-10-31 04:45:32 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+# 启动 OpenVPN
 openvpn --config "$OVPN" --up /etc/openvpn/up.sh --down /etc/openvpn/down.sh --daemon
 
-while ! ip a show tun0; do
+# 静默检测 tun0 接口
+while true; do
+    if ip a show tun0 > /dev/null 2>&1; then
+        echo "tun0 interface is up."
+        break  # 检测到 tun0 后，终止循环
+    fi
     sleep 1
 done
 
+# 启动 sockd
 sockd -f /etc/sockd/sockd.conf -D
